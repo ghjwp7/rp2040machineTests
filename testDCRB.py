928ffdca27cb86f5e3ef0f75a1f8c9fda593d90d
James Waldby <j13@pat7.com>
2025-01-06 08:14:26 -0700

    testDCRB.py comments fixup

--- testDCRB.py
+++ testDCRB.py
@@ -1,24 +1,20 @@
 #!/bin/env python
 # -*- mode: python;  coding: utf-8 -*-  jiw - 5 Jan 2025
-#  RP2040 DMA testing: see if dmaCtrlRegBits.py works
-
+#  RP2040 DMA testing: demo that dmaCtrlRegBits.py functions work ok atm
 import dmaCtrlRegBits as cb
 
 def main():
+    # test that  fieldList()  returns tuple of (name, nbits) pairs
     print('fieldList():', cb.fieldList())
+    # test print of  unpackVals(p)  in a form like:
+    #      unpackVals(00220021): array('b', [1, 0, 0, 0, 1, 0, 0, 0, 4, 1, 0, 0, 0, 0, 0, 0, 0])
     print('                                       sz ir iw         treq         bz    we re ae')
-    # Eg:  unpackVals(00220021): array('b', [1, 0, 0, 0, 1, 0, 0, 0, 4, 1, 0, 0, 0, 0, 0, 0, 0])
-
     for p in (0x1200025, 0x1200029, 0x220021, 0x3f8021, 0x3f8025):
         print(f'unpackVals({p:08x}): {cb.unpackVals(p)}')
     print()
+    # test:  printHeadLines(), printCodes(p)
     cb.printHeadLines()
     for p in (0x1200025, 0x1200029, 0x220021, 0x3f8021, 0x3f8025):
         cb.printCodes(p)
 if __name__ == "__main__":
     main()
-
-# Heading format --
-#'                                       sz ir iw         treq         bz    we re ae'
-# unpackVals(00220021): array('b', [1, 0, 0, 0, 1, 0, 0, 0, 4, 1, 0, 0, 0, 0, 0, 0, 0])
-
